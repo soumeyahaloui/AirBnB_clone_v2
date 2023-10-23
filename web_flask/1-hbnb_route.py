ded21c5ff617df293a560eb470db13cd8d3022cd
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-23 16:09:35 -0700

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Module to start a Flask web application"""
+"""
+Starts a Flask web application that listens on 0.0.0.0, port 5000,
+and defines two routes with strict_slashes=False.
+"""
 
 from flask import Flask
 
@@ -7,12 +10,16 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Route that displays 'Hello HBNB!'"""
+    """
+    Route that displays 'Hello HBNB!'
+    """
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Route that displays 'HBNB'"""
+    """
+    Route that displays 'HBNB'
+    """
     return "HBNB"
 
 if __name__ == "__main__":
