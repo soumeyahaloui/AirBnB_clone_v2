85063a1a71d0ee8a64da2fa3f12647495578c2e5
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-23 16:31:46 -0700

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application that listens on 0.0.0.0, port 5000,
-and defines two routes with strict_slashes=False.
-"""
+""" 1. Script to start a Flask web application with 2 commands """
 
 from flask import Flask
 
+
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """
-    Route that displays 'Hello HBNB!'
-    """
-    return "Hello HBNB!"
+def hello_world():
+    """ Returns some text. """
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """
-    Route that displays 'HBNB'
-    """
-    return "HBNB"
+def hello():
+    """ Return other text. """
+    return 'HBNB'
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
