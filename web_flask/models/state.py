f065ffb4944c1dbbab62a30a2fa2bbecbbeb6fc6
soumeyahaloui <soumeyahaloui@gmail.com>
2023-10-23 16:51:29 -0700

    updated

--- web_flask/models/state.py
+++ web_flask/models/state.py
@@ -1,15 +1,35 @@
-class State(BaseModel, Base):
-    # ... (your existing code)
+#!/usr/bin/python3
+"""This is the state class"""
+import os
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
+
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+        cities = relationship between state and city tables.
+    """
+
+    __tablename__ = 'states'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship(
+            'City', back_populates='state',
+            cascade='all, delete, delete-orphan')
+
+    else:
+        name = ""
+
         @property
         def cities(self):
-            """
-            Getter method to return the list of City objects linked to the current State.
-            """
-            from models import storage
-            city_list = []
-            for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            """returns list of Cities and some relationships"""
+            cities_instances = []
+            cities_dict = models.storage.all(models.City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
